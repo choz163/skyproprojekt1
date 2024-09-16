749aeda980dd1eeb66512e49d88e1043ee55595e
Артем Шошура <artemshoshura@mail.ru>
2024-09-16 23:13:15 +0300

    Изменение функции маскировки карты в masks

--- src/masks.py
+++ src/masks.py
@@ -1,25 +1,28 @@
 def get_mask_card_number(card_number: str) -> str:
     """Функция, которая маскирует номер банковской карты"""
+    hidden_number = card_number[:21] + "** ****" + card_number[-4:]
+    return hidden_number
     # Скрываем символы номера карты
-    hidden_chars = [6, 7, 8, 9, 10, 11]
-    hidden_numbers = ""
-    for number, char in enumerate(card_number):
-        if number in hidden_chars:
-            hidden_numbers += "*"
-        else:
-            hidden_numbers += char
-    # Разбитие номера карты на блоки
-    breaking = [
-        hidden_numbers[0:4],
-        hidden_numbers[4:8],
-        hidden_numbers[8:12],
-        hidden_numbers[12:16],
-    ]
+    # hidden_chars = [20, 21, 22, 23, 24, 25]
+    # hidden_numbers = ""
+    # for number, char in enumerate(card_number):
+    #     if number in hidden_chars:
+    #         hidden_numbers += "*"
+    #     else:
+    #         hidden_numbers += char
+    # # Разбитие номера карты на блоки
+    # breaking = [
+    #     hidden_numbers[0:12],
+    #     hidden_numbers[12:16],
+    #     hidden_numbers[16:20],
+    #     hidden_numbers[20:24],
+    #     hidden_numbers[24:28],
+    # ]
+    #
+    # return " ".join(breaking)
+    # return hidden_numbers
 
-    return " ".join(breaking)
-
-
-print(get_mask_card_number("7000792289606361"))
+# print(get_mask_card_number("Visa Platinum 7000792289606361"))
 
 
 def get_mask_account(account_number: str) -> str:
@@ -27,4 +30,4 @@ def get_mask_account(account_number: str) -> str:
     return f"** {account_number[-4:]}"
 
 
-print(get_mask_account("73654108430135874305"))
+# print(get_mask_account("73654108430135874305"))
