2ab181cec9d7ae4dd6975637c31a889b4ba1b469
Артем Шошура <artemshoshura@mail.ru>
2024-09-25 22:56:03 +0300

    Добавление тестов для модулей masks,widget, processing

--- src/widget.py
+++ src/widget.py
@@ -1,19 +1,14 @@
 from src.masks import get_mask_account, get_mask_card_number
 
 
-def mask_account_card (names_card_and_check: str) -> str:
-    """Функция которая маскирует счет или банковскую карту"""
-    if "Счет" in names_card_and_check:
+def mask_account_card(names_card_and_check: str) -> str:
+    """Функция, которая маскирует счет или банковскую карту"""
+    if "Visa Platinum" in names_card_and_check or "Maestro" in names_card_and_check:
         return get_mask_account(names_card_and_check)
     else:
         return get_mask_card_number(names_card_and_check)
 
 
-print(mask_account_card("Visa Platinum 7000792289606361"))
-
-
 def get_date(input_date: str) -> str:
     """Функция изменения формата даты"""
     return f"{input_date[8:10]}.{input_date[5:7]}.{input_date[0:4]}"
-
-print(get_date("2024-03-11T02:26:18.671407"))
