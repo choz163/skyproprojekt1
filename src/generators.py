843bbfe5846316e7b8699bb4403fddab7db8fd9b
Артем Шошура <artemshoshura@mail.ru>
2024-09-30 20:53:30 +0300

    Добавление настроек для flake8, добавление тестов для generators функций, правка кода и устранение ошибок

--- src/generators.py
+++ src/generators.py
@@ -3,7 +3,6 @@ import sys
 from typing import Any, Generator
 
 
-
 def filter_by_currency(transactions: list, currency_code: str = "USD") -> Generator[Any, Any, Any]:
     """Функция возвращает итератор, который поочередно выдает транзакции, где валюта операции соответствует заданной"""
     # currency_code = (i for i in transactions if i ["operationsAmount "]["currency"]["code"])
@@ -30,5 +29,3 @@ def card_number_generator(start: int, stop: int) -> Generator[str, Any, None]:
         number_zero = "0000000000000000"
         card_number = number_zero[: -len(str(x))] + str(x)
         yield f"{card_number[:4]} {card_number[4:8]} {card_number[8:12]} {card_number[12:]}"
-
-
