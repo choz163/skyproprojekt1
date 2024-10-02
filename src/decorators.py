91be134d57b14553d0f6e7ad5773e9b11aa1dec4
Артем Шошура <artemshoshura@mail.ru>
2024-10-02 21:41:08 +0300

    Выполнение ДЗ 11.2 Декораторы с добавлением двух модулей для записи функции и ее теста, добавление файла mylog.txt для записей логов и обновление redme

---
+++ src/decorators.py
@@ -0,0 +1,28 @@
+from functools import wraps
+
+
+def log(filename=None):
+    """Декоратор автоматически логирует начало и конец выполнения функции,
+    а также ее результаты или возникшие ошибки."""
+    def dekorator(my_function):
+        @wraps(my_function)
+        def wrapper(*args, **kwargs):
+            if not filename:
+                print(f"{my_function.__name__} started")
+                try:
+                    my_function(*args, **kwargs)
+                    print(f"{my_function.__name__} ok")
+                    print(f"{my_function.__name__} finished")
+                except Exception as e:
+                    print(f"{my_function.__name__} error: {e}. Inputs: {args}, {kwargs}")
+            else:
+                try:
+                    my_function(*args, **kwargs)
+                    with open(filename, "w") as file:
+                        file.write(f"{my_function.__name__} ok")
+                except Exception as e:
+                    with open(filename, "w") as file:
+                        file.write(f"{my_function.__name__} error: {e}. Inputs: {args}, {kwargs}")
+        return wrapper
+    return dekorator
+
