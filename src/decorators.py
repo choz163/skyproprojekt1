9aaeb2f3b333fabfd1c5d08f1e08db97578ba048
Артем Шошура <artemshoshura@mail.ru>
2024-10-05 22:51:48 +0300

    испраление функции decorators.py

--- src/decorators.py
+++ src/decorators.py
@@ -7,31 +7,25 @@ def log(filename=None):
     def dekorator(my_func):
         @wraps(my_func)
         def wrapper(*args, **kwargs):
+            result = None
             if not filename:
                 print(f'{my_func.__name__} started')
                 try:
-                    my_func(*args, **kwargs)
+                    result = my_func(*args, **kwargs)
                     print(f'{my_func.__name__} ok')
                     print(f'{my_func.__name__} finished')
                 except Exception as e:
                     print(f'{my_func.__name__} error: {e}. Inputs: {args}, {kwargs}')
             else:
                 try:
-                    my_func(*args, **kwargs)
+                    result = my_func(*args, **kwargs)
                     with open(filename, 'w') as file:
                         file.write(f'{my_func.__name__} ok')
                 except Exception as e:
                     with open(filename, 'w') as file:
                         file.write(f'{my_func.__name__} error: {e}. Inputs: {args}, {kwargs}')
-            # return result
+            return result
         return wrapper
     return dekorator
 
-
-@log()
-def my_function(x, y):
-    return x / y
-
-
-my_function(10, 5)
 
