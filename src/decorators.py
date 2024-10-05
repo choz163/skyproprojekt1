d98cc017d9dcf54bb535df171b0621510d0d2b48
Артем Шошура <artemshoshura@mail.ru>
2024-10-05 21:32:34 +0300

    крайняя правка 11.2 дз

--- src/decorators.py
+++ src/decorators.py
@@ -1,26 +1,37 @@
+from functools import wraps
+
+
 def log(filename=None):
     """Декоратор автоматически логирует начало и конец выполнения функции,
     а также ее результаты или возникшие ошибки."""
-
-    def dekorator(my_function):
+    def dekorator(my_func):
+        @wraps(my_func)
         def wrapper(*args, **kwargs):
             if not filename:
-                print(f"{my_function.__name__} started")
+                print(f'{my_func.__name__} started')
                 try:
-                    my_function(*args, **kwargs)
-                    print(f"{my_function.__name__} ok")
-                    print(f"{my_function.__name__} finished")
+                    my_func(*args, **kwargs)
+                    print(f'{my_func.__name__} ok')
+                    print(f'{my_func.__name__} finished')
                 except Exception as e:
-                    print(f"{my_function.__name__} error: {e}. Inputs: {args}, {kwargs}")
+                    print(f'{my_func.__name__} error: {e}. Inputs: {args}, {kwargs}')
             else:
                 try:
-                    my_function(*args, **kwargs)
-                    with open(filename, "w") as file:
-                        file.write(f"{my_function.__name__} ok")
+                    my_func(*args, **kwargs)
+                    with open(filename, 'w') as file:
+                        file.write(f'{my_func.__name__} ok')
                 except Exception as e:
-                    with open(filename, "w") as file:
-                        file.write(f"{my_function.__name__} error: {e}. Inputs: {args}, {kwargs}")
-
+                    with open(filename, 'w') as file:
+                        file.write(f'{my_func.__name__} error: {e}. Inputs: {args}, {kwargs}')
+            # return result
         return wrapper
-
     return dekorator
+
+
+@log()
+def my_function(x, y):
+    return x / y
+
+
+my_function(10, 5)
+
